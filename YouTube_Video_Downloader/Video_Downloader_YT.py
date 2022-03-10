3bc31d3fb42a929a80b42039bf6217709f480ffe
ChanHT <chanhts323@gmail.com>
2022-03-10 23:52:21 +0800

    Update Video_Downloader_YT.py

--- YouTube_Video_Downloader/Video_Downloader_YT.py
+++ YouTube_Video_Downloader/Video_Downloader_YT.py
@@ -1,5 +1,5 @@
 from pytube import YouTube
-link = 'https://www.youtube.com/watch?v=WDyoYrNz7VY'
+link = '<Which link you want to download?>'
 yt = YouTube(link)
 yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
 print('download finished..', link)
